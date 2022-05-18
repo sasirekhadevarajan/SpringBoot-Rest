ba44b34cfc8996f2e3bd5d3b4236192e2c480cf6
sasirekhadevarajan <sasirekha.devarajan@gmail.com>
2022-05-18 11:04:18 -0500

    Simple Hello World

---
+++ src/main/java/com/springboot/rest/controller/UserDetails.java
@@ -0,0 +1,63 @@
+package com.springboot.rest.controller;
+
+public class UserDetails {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	
+	public UserDetails() {
+		super();
+	}
+	
+	
+	public UserDetails(String firstName, String lastName, String email, String phone) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	@Override
+	public String toString() {
+		return "UserDetails [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", phone="
+				+ phone + "]";
+	}
+
+	
+}
